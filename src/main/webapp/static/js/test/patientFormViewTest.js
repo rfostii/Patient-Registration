5a3cea55692805af814bf7a1689a27a16946cacb
ruslan <fostiiruslan2706@gmail.com>
2014-02-14 18:10:42 +0200

    add test for patient form view

--- src/main/webapp/static/js/test/patientFormViewTest.js
+++ src/main/webapp/static/js/test/patientFormViewTest.js
@@ -12,6 +12,18 @@ define([
                 model: new PatientModel()
             });
         });
+
+        it("should append form to $el", function() {
+            this.patientFormView.render();
+            expect(this.patientFormView.$el.find('form'))
+                .toBeDefined();
+        });
+
+        it("validationForm method should be implemented", function() {
+            expect(this.patientFormView.validationForm(new PatientModel().toJSON()))
+                .toEqual(this.patientFormView);
+        });
+
         it("validationForm method should be implemented", function() {
             expect(this.patientFormView.validationForm(new PatientModel().toJSON()))
                 .toEqual(this.patientFormView);
